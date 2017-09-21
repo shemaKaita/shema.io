b25910dd7824c0df00b39b40d947e8c884a8cbc3
shema <skaitakirwa@gmail.com>
2017-09-21 22:28:33 +0200

    fresh base

--- public/dist/js/app.bundle.js
+++ public/dist/js/app.bundle.js
@@ -1,50 +1,9 @@
-webpackJsonp([0],{
-
-/***/ 2:
+webpackJsonp([0],[
+/* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-var _jquery = __webpack_require__(0);
-
-var _jquery2 = _interopRequireDefault(_jquery);
-
-var _uikit = __webpack_require__(1);
-
-var _uikit2 = _interopRequireDefault(_uikit);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Shema = function () {
-	function Shema(message) {
-		_classCallCheck(this, Shema);
-
-		this.msg = message;
-	}
-
-	_createClass(Shema, [{
-		key: 'greet',
-		value: function greet() {
-			return 'Hello ' + this.msg;
-		}
-	}]);
-
-	return Shema;
-}();
-
-var greeting = new Shema("Universe!!");
-
-(0, _jquery2.default)(document).ready(function ($) {
-	var message = greeting.greet();
-	$('h1').text(message);
-	console.log(_uikit2.default);
-});
-
 /***/ })
-
-},[2]);
+],[0]);
