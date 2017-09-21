cbc4cb0fb270491f25496fdda995d60bae754110
shema <skaitakirwa@gmail.com>
2017-09-21 23:21:20 +0200

    updated build process and setup for jquery dependant modules

--- public/assets/js/app/index.js
+++ public/assets/js/app/index.js
@@ -1,20 +1,9 @@
 import jQuery from 'jquery';
-import uikit from 'uikit';
 
-
-class Shema{
-	constructor(message){
-		this.msg = message;
-	}
-	greet(){
-		return (`Hello ${this.msg}`);
-	}
-}
-
-const greeting = new Shema("Universe!!");
+// Allow other scripts access to jquery...
+window.$ = jQuery,
+window.jQuery = jQuery;
 
 jQuery(document).ready(($) => {
-	const message = greeting.greet();
-	$('h1').text(message);
-	console.log(uikit); 
+	// Code...
 });
