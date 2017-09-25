c2ebac49de361f714e8447bba44d0d7fb66ff660
shema <skaitakirwa@gmail.com>
2017-09-26 00:19:02 +0200

    started css structure and added basic gulp process for sass and minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp');
+var gulpSass = require('gulp-sass');
+var gulpConcat = require ('gulp-concat');
+var gulpCleanCss = require('gulp-clean-css');
 var webpack = require('webpack-stream');
 var browserSync = require("browser-sync").create();
 var webpack_config = require('./webpack.config.js');
 var settings = require('./settings.json');
+var assets = 'public/assets/';
+var dist = 'public/dist/';
 
 gulp.task('webpack', function(){
 	return webpack(webpack_config)
@@ -10,13 +15,26 @@ gulp.task('webpack', function(){
 			.pipe(browserSync.stream());
 });
 
+gulp.task('sass:core', function(){
+	return gulp.src(`${assets}css/app/core/core.scss`)
+			.pipe(gulpSass())
+			.pipe(gulpCleanCss({compatibility: 'ie8'}))
+			.pipe(gulp.dest('public/dist/css/'))
+			.pipe(browserSync.stream())
+});
+
+gulp.task('sass:watch', ['sass:core'], function(){
+	gulp.watch(`${assets}css/app/**/*.scss`, ['sass:core']);
+	gulp.watch(`${dist}css/*.css`).on('change', browserSync.reload);
+})
+
 gulp.task('serve', ['webpack'], function(){
 	browserSync.init({
         proxy: `${settings.proxy.url}`,
     });
 	
-	gulp.watch('public/assets/js/app/**/*.js', ['webpack']);
-	gulp.watch('public/dist/js/app.bundle.js').on('change', browserSync.reload);
+	gulp.watch(`${assets}js/app/**/*.js`, ['webpack']);
+	gulp.watch(`${dist}js/app.bundle.js`).on('change', browserSync.reload);
 });
-gulp.task('default', ['serve']);
+gulp.task('default', [ 'serve', 'sass:watch']);
 
