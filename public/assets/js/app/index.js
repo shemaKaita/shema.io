8c9b52f89724d89b668dcc5d7bf33df8ddddd677
shema <skaitakirwa@gmail.com>
2017-10-16 14:31:30 +0200

    new mockup draft

--- public/assets/js/app/index.js
+++ public/assets/js/app/index.js
@@ -4,7 +4,5 @@ window.$ = jQuery,
 window.jQuery = jQuery;
 
 $(document).ready(($) => {
-	let blah = [0,1,2,3,4];
-
-	console.log(...blah, 5, 6, 7);
+	
 });
--- public/dist/js/app.bundle.js
+++ public/dist/js/app.bundle.js
@@ -10,13 +10,7 @@ var jQuery = __webpack_require__(0);
 
 window.$ = jQuery, window.jQuery = jQuery;
 
-$(document).ready(function ($) {
-	var _console;
-
-	var blah = [0, 1, 2, 3, 4];
-
-	(_console = console).log.apply(_console, blah.concat([5, 6, 7]));
-});
+$(document).ready(function ($) {});
 
 /***/ })
 ],[1]);
